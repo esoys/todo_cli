debc9a67aec68e1d8db41652cca9735c22d5833d
esoys <erik@soysal.de>
2025-12-11 19:40:32 +0100

    fixed list

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int main() {
   test_task.done = false;
   printf("task: %s\n", test_task.task);
 
-  Todo_List todo_list;
-  todo_list.todos[0] = test_task;
-  printf("todos: %s\n", todo_list.todos[0].task);
+  Todo todos[3];
+  todos[0] = test_task;
+  printf("list item 1: %s\n", todos[0].task);
   return 0;
 }
