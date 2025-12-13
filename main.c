aa40757edc8472c8163aaeb565f73dfe410c4a41
esoys <erik@soysal.de>
2025-12-13 21:54:14 +0100

    add user input

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include "todo.h"
 
+/*zum compiliern header mit einbeziehen
+  -> gcc main.c todo.c -o main
+*/
 
 int main() {
 
-  Todo test = create_todo("Foo, Bar, Baz", 1);
+  printf("Task eingeben: ");
+  char task[200];
+  fgets(task, sizeof(task), stdin);
+  printf("input: %s\n", task);
+
+  Todo test = create_todo(task, 1);
   printf("test todo task: %s\n", test.task);
   printf("test todo id: %d\n", test.id);
   printf("test todo id: %s\n", test.done ? "true" : "false");
